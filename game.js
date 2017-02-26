22b89d313618c5327cf214e6c30ffdea1666b329
Sjoerd <svanhouten@gmail.com>
2017-02-26 17:35:01 +0100

    moved the getScore methods to game, added method to access each getScore through input of string: e.g.: "FullHouse" gives game.getScoreAsFullHouse();

--- game.js
+++ game.js
@@ -1,12 +1,78 @@
 function YahtzyGame(names, visualiser, scoreChecker){
     var dice = []
-
+//    var scoreTypes = ["Ones","Twos","Threes","Fours","Fives","Sixes","ThreeOfAKind","FourOfAKind","Yahtzy","FullHouse","SmallStraight","LargeStraight","Chance"]
     this.selectInput = function(choice){
-        scoreChecker.inputDice(dice)
-        return scoreChecker.getScoreAsLargeStraight(dice);
+        scoreChecker.inputDice(dice);
+        return this["getScoreAs"+choice]();
     };
     this.rollDice = function(input){
          dice = input;
     };
-}
 
+    this.getScoreAsThreeOfAKind = function(){
+        if(scoreChecker.isThreeOfAKind()){
+        return scoreChecker.giveAllDiceValuesAddedUp();
+        } else{return 0};
+    };
+    this.getScoreAsFourOfAKind = function(){
+        if(scoreChecker.isFourOfAKind()){
+        return scoreChecker.giveAllDiceValuesAddedUp();
+        } else{return 0};
+    };
+    this.getScoreAsFullHouse = function() {
+         if (scoreChecker.isFullHouse()){
+                return 25 , scoreChecker.addScoreBottomHalf(25);
+            }
+            else {
+                return 0;
+            }
+    };
+    this.getScoreAsSmallStraight = function() {
+         if (scoreChecker.isSmallStraight()){
+                return 30;
+            }
+            else {
+                return 0;
+            }
+    };
+    this.getScoreAsLargeStraight = function() {
+         if (scoreChecker.isLargeStraight()){
+                return 40;
+            }
+            else {
+                return 0;
+            }
+    };
+    this.getScoreAsYahtzy = function() {
+         if (scoreChecker.isYahtzy()){
+                return 50;
+            }
+            else {
+                return 0;
+            }
+    };
+    this.getScoreAsChance = function() {
+         return scoreChecker.giveAllDiceValuesAddedUp();
+    };
+    this.getScoreAsOnes = function() {
+         return scoreChecker.giveAddedUpXs(0);
+    };
+    this.getScoreAsTwos = function() {
+         return scoreChecker.giveAddedUpXs(1);
+    };
+    this.getScoreAsThrees = function() {
+         return scoreChecker.giveAddedUpXs(2);
+    };
+    this.getScoreAsFours = function() {
+         return scoreChecker.giveAddedUpXs(3);
+    };
+    this.getScoreAsFives = function() {
+         return scoreChecker.giveAddedUpXs(4);
+    };
+    this.getScoreAsSixes = function() {
+         return scoreChecker.giveAddedUpXs(5);
+    };
+    this.checkForIncorrectInput = function() {
+         return scoreChecker.checkForWrongNumbers();
+    };
+}
